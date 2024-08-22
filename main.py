e019edeb5b38fb9defd572a2114df51431f6e242
wifiskeleton <ballswow500@gmail.com>
2024-08-22 18:22:12 -0400

    STUFFAND SHIT MNANNM !!

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = ',')
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix='your prefix', intents=discord.Intents.all()) 
 
 @client.event
 async def on_ready():
@@ -12,6 +15,24 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello, i kill niggers!")
+
+@client.command()
+async def niggers(ctx):
+    await ctx.send("did you know black people cause 54% of rapes?")
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(1275670027437342741)
+    await channel.send("whats up nigger, welcome to the chudverse")
+
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(1275670027437342741)
+    await channel.send("Goodbyefaggot.")
+
+
+
 
 
 client.run('MTI3NjA0Mzg2MjExMjA3NTc4Nw.GpJtrl.lxBWI5BLfk4wiNkNJlFa_v9IqUkKSKLLxpjBkY')
